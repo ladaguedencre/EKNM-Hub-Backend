667f88a56d9bcb095bed445766d48879e1606aaa
ladaguedencre <valerarezz@gmail.com>
2023-07-04 19:03:41 +0200

    Update settings to release

--- common.py
+++ common.py
@@ -4,4 +4,4 @@ import os
 MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb')
 MONGODB_PORT = int(os.getenv('MONGODB_PORT', 27017))
 common_client = MongoClient(MONGODB_URL, port=MONGODB_PORT)
-common_db = common_client.test_database
+common_db = common_client.hub_db
--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def page_restore():
     return restore()
     
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=False, host='0.0.0.0', port=5003)

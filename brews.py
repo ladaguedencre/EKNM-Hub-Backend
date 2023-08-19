ecf466eabcd2500b26d05629b17ca0321c5d4438
ladaguedencre <valerarezz@gmail.com>
2023-08-19 14:44:48 +0200

    Remove restore and projects methods, add api prefix

--- brews.py
+++ brews.py
@@ -5,14 +5,12 @@ from common import common_db
 @dataclass
 class Brew:
     id: str
-    count: int
     description: any
     image_url: str
     name: any
 
-    def __init__(self, id: str, count: int, description: any, image_url: str, name: any):
+    def __init__(self, id: str, description: any, image_url: str, name: any):
         self.id = id
-        self.count = count
         self.description = description
         self.image_url = image_url
         self.name = name
@@ -27,7 +25,6 @@ def get_brews() -> List[Brew]:
         results.append(
             Brew(
                 item['id'],
-                item['count'],
                 item['description'],
                 item['image_url'],
                 item['name']
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask, abort
 from flask_cors import CORS
 from bindings import get_bindings
-from backups import restore
 from goods import get_goods
 from brews import get_brews
-from projects import get_projects
 from articles import get_article
 
 app = Flask(__name__)
@@ -14,32 +12,24 @@ CORS(app)
 def home():
     return "EKNM Hub Backend. Leave if you don't know what you are doing."
     
-@app.route("/bindings")
+@app.route("/api/bindings")
 def bindings():
     return get_bindings()
 
-@app.route("/goods")
+@app.route("/api/goods")
 def goods():
     return get_goods()
 
-@app.route("/brews")
+@app.route("/api/brews")
 def brews():
     return get_brews()
 
-@app.route("/projects")
-def projects():
-    return get_projects()
-
-@app.route('/articles/<id>')
+@app.route('/api/articles/<id>')
 def article(id: str):
     article = get_article(id)
     if not article:
         abort(404)
     return article
-
-@app.route("/restore")
-def page_restore():
-    return restore()
     
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5003)

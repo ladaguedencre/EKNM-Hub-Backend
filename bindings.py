a2bf24aa079fbea29616f6a6c67e1fbd1c662d0e
ladaguedencre <valerarezz@gmail.com>
2023-09-02 16:43:18 +0200

    Update bindings script to parse articles collection

--- bindings.py
+++ bindings.py
@@ -2,45 +2,41 @@ from dataclasses import dataclass
 from typing import List
 from common import common_db
 
+
 @dataclass
 class Binding:
     id: str
-    first_line: str
-    second_line: str
-    image_url: str
-    link: str
+    title: str
+    subtitle: str
+    author: str
+    date: str
+    category: str
 
-    def __init__(self, id: str, first_line: str, second_line: str, image_url: str, link: str):
+    def __init__(self, id: str, title: str, subtitle: str, author: str, date: str, category: str):
         self.id = id
-        self.first_line = first_line
-        self.second_line = second_line
-        self.image_url = image_url
-        self.link = link
+        self.title = title
+        self.subtitle = subtitle
+        self.author = author
+        self.date = date
+        self.category = category
 
-
-mock_binding: List[Binding] = [
-    Binding(
-        'test1',
-        'Luntik test 1',
-        'Super test 228',
-        '../../assets/mock/icon1.png',
-        '/test1',
-    )
-]
 
 def get_bindings() -> List[Binding]:
-    collection = common_db.bindings
+    collection = common_db.articles
 
     results = []
 
     for item in collection.find({}):
+        if 'public' in item and item['public'] == False:
+            continue
         results.append(
             Binding(
                 item['id'],
-                item['first_line'],
-                item['second_line'],
-                item['image_url'],
-                item['link']
+                item['title'],
+                item['subtitle'],
+                item['author'],
+                item['date'],
+                item['category']
             )
         )
 

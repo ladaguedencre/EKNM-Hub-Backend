acef87c8e4035a9273aa1689120f84e93cc7bfcc
ladaguedencre <valerarezz@gmail.com>
2023-09-05 21:16:58 +0200

    Add script for converting article content

---
+++ utils/md_to_line.py
@@ -0,0 +1,17 @@
+# Read the input Markdown file
+input_file_path = 'input.md'
+output_file_path = 'output.md'
+
+with open(input_file_path, 'r') as input_file:
+    # Read the content of the input file
+    markdown_content = input_file.read()
+
+# Replace characters
+markdown_content = markdown_content.replace('\n', '\\n')
+markdown_content = markdown_content.replace('\'', '\\\'')
+
+# Write the modified content to the output file
+with open(output_file_path, 'w') as output_file:
+    output_file.write(markdown_content)
+
+print(f"Converted {input_file_path} to a one-line Markdown file: {output_file_path}")
